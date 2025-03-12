f54b61ea8747fb7179faa32b19179c8b8e5e590b
Ferdinand Leybach <ferdinand@leybach.de>
2025-03-12 10:42:15 +0100

    Adding the final result of the program to get an idea on how to structure the input.

--- src/developer.c
+++ src/developer.c
@@ -1,1 +1,18 @@
 #include "developer.h"
+/*
+
+***********************
+=====================
+Developer: 
+name: Selina Kyle
+alias: Catwoman
+=====================
+=====================
+Developer: 
+name: Clark Kent
+alias: Superman
+=====================
+***********************
+
+
+*/
