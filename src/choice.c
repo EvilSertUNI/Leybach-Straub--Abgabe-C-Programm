60c375dd569bdc3a81c05e5167d17c0e5c5e71df
SimonStraub2005 <straub.simon-it24@it.dhbw-ravensburg.de>
2025-03-14 10:10:24 +0000

    Edited the coice.c file to work as the main_menu funcion. Variable / function naming not finished / just for testing

---
+++ src/choice.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+//#include "choice.h"
+
+    
+
+void main_menu(int const *input_ptr)
+{
+    
+
+
+    printf("\033[4;92m========================\n
+    Choose your action: \n
+    List Developers \t[1]\n
+    Print Group Logo \t[2]\n
+    Print Group \t\t[3]\n
+    Exit \t\t\t[4]\n
+    ========================\033[0m\n
+    Enter your selection: ");
+
+
+        int user_input;
+        printf("Enter a value: ")
+        scanf("%d", &user_input);
+        input_ptr = user_input;
+
+        switch *input_ptr
+        {
+        case 1:
+            //developer.c
+            printf("case1");
+            break;
+        case 2:
+            //group_logo.c
+            printf("case2");
+            break;    
+        case 3:
+            //group.c
+            printf("case3");
+            break;
+        case 4:
+            //Exit the program
+            printf("case4");
+            break;
+            }
+    //Test the function with sample_values
+
+    int const * test_ptr;
+    int test_coice = 4;
+    test_ptr = &test_coice;
+    main_menu(test_ptr);
+}            
+
+
